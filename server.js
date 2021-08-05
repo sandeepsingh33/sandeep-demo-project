0c2ef536de31b79a4d88208d8b20d9d1ed3574d4
sandeepsingh33 <31407368+sandeepsingh33@users.noreply.github.com>
2021-08-05 15:01:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http=require('http')
 const server=http.createServer((req,res)=>{
     console.log('New connection')
-    res.end('This is Sandeep, working on my first GCP-Cloud Run and Cloud Build')
+    res.end('This is Sandeep, working on my first GCP-Cloud Run and Cloud Build-updated in git')
 })
 const PORT=process.env.PORT || 8080
 server.listen(PORT, ()=>console.log('Listening'))
