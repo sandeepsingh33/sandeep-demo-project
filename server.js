fa0d1e3c7ff64128679bf968acd51311fa63c117
sandeepsingh33 <31407368+sandeepsingh33@users.noreply.github.com>
2021-08-05 20:21:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ var server = http.createServer(function (req, res) {   //create web server
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
         
         // set response content    
-        res.write('<html><body><p>This is home Page.</p></body></html>');
+        res.write('<html><body><h1>This is home Page.</h1></body></html>');
         res.end();
     
     }
     else if (req.url == "/student") {
         
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is student Page.</p></body></html>');
+        res.write('<html><body><h2>This is student Page.</h2></body></html>');
         res.end();
     
     }
     else if (req.url == "/admin") {
         
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
+        res.write('<html><body><h3>This is admin Page.</h3></body></html>');
         res.end();
     
     }
