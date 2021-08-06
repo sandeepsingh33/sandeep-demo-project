9411c374573b2e61353c40a5e50a5e3c54a22d6b
sandeepsingh33 <31407368+sandeepsingh33@users.noreply.github.com>
2021-08-06 12:59:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -45,4 +45,4 @@ var server = http.createServer(function (req, res) {   //create web server
 
 server.listen(8080); //6 - listen for any incoming requests
 
-console.log('Node.js web server at port 5000 is running..')
+console.log('Node.js web server at port 8080 is running..')
